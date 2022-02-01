0552ec7d8d753fcef64dbc4a38e65e512564fffc
chandani123 <40708857+chandani123@users.noreply.github.com>
2022-02-01 17:05:51 +1000

    SetUp Webdriver

---
+++ src/inteliHR/Basic.java
@@ -0,0 +1,16 @@
+package inteliHR;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basic  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "/Users/chandanibhalala/Downloads/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
